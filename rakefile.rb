b6982b4940782c9bcef417eb5dda08e84215aeca
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-06-22 15:11:55 -0500

    Fail fast if a dependency directory is not found

--- rakefile.rb
+++ rakefile.rb
@@ -4,10 +4,19 @@ code_root = File.expand_path(File.join(ripple_root, "../"))
 
 task :ripple => [:clean] do
   puts "rippling #{ripple_root} to #{code_root} from #{package_root}"
+
   libs = ['fubucore', 'bottles', 'fubumvc', 'fubuvalidation', 'fubufastpack']
+
+  libs.each do |lib|
+    lib_dir = File.join code_root, lib
+    raise "Could not find dependent library: #{lib_dir}" unless File.directory?(lib_dir)
+  end
+
   libs.each do |lib|
     run_in File.join(code_root, lib), "rake nuget:pull nuget:ripple \"NUGET_HUB=#{package_root}\""
   end
+
+  run_in File.join(code_root, "blue"), "rake nuget:pull \"NUGET_HUB=#{package_root}\""
 end
 
 task :clean do
@@ -15,6 +24,5 @@ task :clean do
 end
 
 def run_in(working_dir, cmd)
-  raise "Could not find #{working_dir} - aborting" unless File.directory? working_dir
   sh "run_in_path.cmd #{working_dir.gsub('/','\\')} #{cmd}"
 end
