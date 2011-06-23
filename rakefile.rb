15ee03d368fa631f26580ad5508499faf0e3f6b8
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-06-23 17:49:37 -0500

    Added ripple:start

--- rakefile.rb
+++ rakefile.rb
@@ -1,28 +1,45 @@
+require 'buildsupport/nuget.rb'
 ripple_root = File.dirname(__FILE__)
 package_root = File.join(ripple_root, "packages")
 code_root = File.expand_path(File.join(ripple_root, "../"))
 
-task :ripple => [:clean] do
-  puts "rippling #{ripple_root} to #{code_root} from #{package_root}"
+namespace :ripple do
+  task :start => [:reset] do
+    puts "rippling #{ripple_root} to #{code_root} from #{package_root}"
 
-  libs = ['fubucore', 'bottles', 'fubumvc', 'fubuvalidation', 'fubufastpack']
+    # 1) Determine dependency graph
+    libs = ['fubucore', 'bottles', 'fubumvc', 'fubuvalidation', 'fubufastpack']
 
-  libs.each do |lib|
-    lib_dir = File.join code_root, lib
-    raise "Could not find dependent library: #{lib_dir}" unless File.directory?(lib_dir)
+    # 2) Validate that all dependencies are available locally
+    libs.each do |lib|
+      lib_dir = File.join code_root, lib
+      raise "Could not find dependent library: #{lib_dir}" unless File.directory?(lib_dir)
+    end
+
+    # 3) Write list of jobs
+    init_ripple_jobs libs
+
+    # 4) Start the first job
+    Rake::Task["ripple:nextjob"].execute
+
+  #  libs.each do |lib|
+  #    run_in File.join(code_root, lib), "rake nuget:pull nuget:ripple \"NUGET_HUB=#{package_root}\""
+  #  end
+
+  #  run_in File.join(code_root, "blue"), "rake nuget:pull \"NUGET_HUB=#{package_root}\""
   end
 
-  libs.each do |lib|
-    run_in File.join(code_root, lib), "rake nuget:pull nuget:ripple \"NUGET_HUB=#{package_root}\""
+  task :nextjob do
+    next_job = get_next_job 
+    if next_job
+      run_in File.join(code_root, next_job), "rake ripple:public \"NUGET_HUB=#{package_root}\""
+    else
+      puts "Ripple complete."
+    end
   end
 
-  run_in File.join(code_root, "blue"), "rake nuget:pull \"NUGET_HUB=#{package_root}\""
+  task :reset do
+    rm_rf Dir["#{package_root}/*"], :verbose => false
+  end
 end
 
-task :clean do
-  rm_rf Dir["#{package_root}/*"], :verbose => false
-end
-
-def run_in(working_dir, cmd)
-  sh "run_in_path.cmd #{working_dir.gsub('/','\\')} #{cmd}"
-end
