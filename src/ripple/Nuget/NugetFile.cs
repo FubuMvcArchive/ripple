6feacdda1969d75175f79cd980add7ce006e8cbf
Bob Pace <bob.pace@gmail.com>
2013-04-23 13:24:10 -0600

    replaced version parsing with a regular expression

--- src/ripple/Nuget/NugetFile.cs
+++ src/ripple/Nuget/NugetFile.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
+using System.Text.RegularExpressions;
 using FubuCore;
 using FubuCore.Descriptions;
 using NuGet;
@@ -11,71 +9,48 @@ using ripple.Model;
 
 namespace ripple.Nuget
 {
-	public class NugetName
-	{
-		public string Name { get; private set; }
-		public SemanticVersion Version { get; private set; }
+    public class NugetName
+    {
+        public string Name { get; private set; }
+        public SemanticVersion Version { get; private set; }
 
-		public static NugetName Parse(string input)
-		{
-			var index = 0;
-			var periodFound = false;
+        private static readonly Regex PackageVersionPattern = new Regex(@"(.*?)\.((?<!\w)[\d.]+(?:.*)).nupkg",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-			for (var i = 0; i < input.Length; i++)
-			{
-				var character = input[i];
-				if (character == '.')
-				{
-					periodFound = true;
-					continue;
-				}
+        public static NugetName Parse(string input)
+        {
+            var matches = PackageVersionPattern.Match(input);
+            if (!matches.Success) throw new InvalidOperationException("Invalid package name");
 
-                if (char.IsLetter(character))
-                {
-                    periodFound = false;
-                    continue;
-                }
-
-				if (periodFound && char.IsNumber(character))
-				{
-					index = i;
-					break;
-				}
-			}
-
-			if (index == 0)
-			{
-				throw new InvalidOperationException("Invalid package name");
-			}
-
-			return new NugetName
-			{
-				Name = input.Substring(0, index - 1),
-				Version = SemanticVersion.Parse(input.Substring(index))
-			};
-		}
-	}
+            return new NugetName
+            {
+                Name = matches.Groups[1].Value,
+                Version = SemanticVersion.Parse(matches.Groups[2].Value)
+            };
+        }
+    }
 
     public class NugetFile : INugetFile, DescribesItself
     {
         private readonly string _path;
-	    private readonly SolutionMode _mode;
+        private readonly SolutionMode _mode;
 
-	    public NugetFile(string path, SolutionMode mode)
+        public NugetFile(string path, SolutionMode mode)
         {
             _path = path;
-	        _mode = mode;
+            _mode = mode;
 
-	        var file = Path.GetFileNameWithoutExtension(path);
-	        var result = NugetName.Parse(file);
+            var fileName = Path.GetFileName(_path);
 
-	        Name = result.Name;
-	        Version = result.Version;
-	        IsPreRelease = Version.SpecialVersion.IsNotEmpty();
+            var result = NugetName.Parse(fileName);
+
+            Name = result.Name;
+            Version = result.Version;
+            IsPreRelease = Version.SpecialVersion.IsNotEmpty();
         }
 
-		public string FileName { get { return _path; } }
-	    public string Name { get; private set; }
+        public string FileName { get { return _path; } }
+        public string Name { get; private set; }
         public SemanticVersion Version { get; private set; }
         public bool IsPreRelease { get; private set; }
 
@@ -84,24 +59,24 @@ namespace ripple.Nuget
             return string.Format("Name: {0}, Version: {1}, IsPreRelease: {2}", Name, Version, IsPreRelease);
         }
 
-		public virtual string ExplodedDirectory(string directory)
-		{
-			if(_mode == SolutionMode.Classic)
-				return directory.AppendPath(Name + "." + Version);
+        public virtual string ExplodedDirectory(string directory)
+        {
+            if (_mode == SolutionMode.Classic)
+                return directory.AppendPath(Name + "." + Version);
 
-			return directory.AppendPath(Name).ToFullPath();
-		}
+            return directory.AppendPath(Name).ToFullPath();
+        }
 
-		public string NugetFolder(Solution solution)
-		{
-			return ExplodedDirectory(solution.PackagesDirectory());
-		}
+        public string NugetFolder(Solution solution)
+        {
+            return ExplodedDirectory(solution.PackagesDirectory());
+        }
 
         public IPackage ExplodeTo(string directory)
         {
-	        var explodedDirectory = ExplodedDirectory(directory);
+            var explodedDirectory = ExplodedDirectory(directory);
 
-			RippleLog.Info("Exploding to " + explodedDirectory);
+            RippleLog.Info("Exploding to " + explodedDirectory);
 
             var fileSystem = new FileSystem();
             fileSystem.CreateDirectory(explodedDirectory);
@@ -109,10 +84,11 @@ namespace ripple.Nuget
 
             var package = new ZipPackage(_path);
 
-            package.GetFiles().Each(file => {
+            package.GetFiles().Each(file =>
+            {
                 var target = explodedDirectory.AppendPath(file.Path);
                 fileSystem.CreateDirectory(target.ParentDirectory());
-                
+
                 using (var stream = new FileStream(target, FileMode.Create, FileAccess.Write))
                 {
                     file.GetStream().CopyTo(stream);
@@ -121,25 +97,25 @@ namespace ripple.Nuget
 
             fileSystem.CopyToDirectory(_path, explodedDirectory);
 
-			fileSystem.DeleteFile(_path);
+            fileSystem.DeleteFile(_path);
 
-	        var newFile = Path.Combine(explodedDirectory, Path.GetFileName(_path));
-			return new ZipPackage(newFile);
+            var newFile = Path.Combine(explodedDirectory, Path.GetFileName(_path));
+            return new ZipPackage(newFile);
         }
 
-	    public INugetFile CopyTo(string directory)
-	    {
-		    var target = Path.Combine(directory, Path.GetFileName(_path));
-			new FileSystem().Copy(_path, target);
+        public INugetFile CopyTo(string directory)
+        {
+            var target = Path.Combine(directory, Path.GetFileName(_path));
+            new FileSystem().Copy(_path, target);
 
-		    return new NugetFile(target, _mode);
-	    }
+            return new NugetFile(target, _mode);
+        }
 
 
-	    public void Describe(Description description)
-	    {
-		    description.Title = Name;
-		    description.ShortDescription = "Version: {0}, IsPreRelease: {1}".ToFormat(Version, IsPreRelease);
-	    }
+        public void Describe(Description description)
+        {
+            description.Title = Name;
+            description.ShortDescription = "Version: {0}, IsPreRelease: {1}".ToFormat(Version, IsPreRelease);
+        }
     }
 }
