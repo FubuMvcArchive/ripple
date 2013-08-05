ee25a3377aa3ee46be405ead7473e896350bd4af
jeremydmiller <jeremydmiller@yahoo.com>
2013-08-05 08:05:45 -0500

    redid the gem support in rake, ignoring vshost files, closes GH-147

--- rakefile.rb
+++ rakefile.rb
@@ -24,9 +24,63 @@ end
 	sln.ripple_enabled = true
 	sln.fubudocs_enabled = true
 	
-	sln.ci_steps = ["gem:archive"]
+	sln.ci_steps = [:archive_gem]
 end
 
-BUILD_NUMBER = @solution.options[:build_number]
+desc "Replaces the existing installed gem with the new version for local testing"
+task :local_gem => [:create_gem] do
+	puts 'THIS TASK DOES NOT UNINSTALL OLD VERSIONS!'
+	#sh 'gem uninstall ripple-cli -ax'
+	Dir.chdir 'pkg' do
+	    sh 'gem install ripple-cli'
+    end
+end
 
-load File.expand_path('../ripple-cli/Rakefile', __FILE__)
+desc "Moves the gem to the archive folder"
+task :archive_gem => [:create_gem] do
+	copyOutputFiles "pkg", "*.gem", "artifacts"
+end
+
+
+desc "Creates the gem for fubu.exe"
+task :create_gem => [:compile] do
+    require "rubygems/package"
+	cleanDirectory 'bin';	
+	cleanDirectory 'pkg'
+	
+	copyOutputFiles "src/ripple/bin/#{@solution.compilemode}", '*.dll', 'bin'
+	copyOutputFiles "src/ripple/bin/#{@solution.compilemode}", 'ripple.exe', 'bin'
+	copyOutputFiles "src/ripple/bin/#{@solution.compilemode}", 'run-git.cmd', 'bin'
+	
+	FileUtils.copy 'ripple', 'bin'
+
+
+	spec = Gem::Specification.new do |s|
+		s.name          = "ripple-cli"
+		s.version       = @solution.options[:build_number]
+		s.authors       = ["Jeremy Miller", "Josh Arnold", "Ryan Rauh", "Matthew Smith"]
+		s.email         = ["fubumvc-devel@googlegroups.com"]
+		s.description   = "Ripple is a tool that wraps Nuget with workflow more conducive to upstream/downstream development across code repositories"
+		s.summary       = "Improved dependency management with Nuget"
+		s.homepage      = "http://fubu-project.org"
+		s.license       = "Apache 2.0"
+
+		s.files         = Dir.glob("bin/**/*").to_a
+		s.executables   = ["ripple"]
+		s.require_paths = ["lib"]
+
+		s.add_development_dependency "bundler", "~> 1.3.5"
+		s.add_development_dependency "rake"
+	end  
+	
+    puts "ON THE FLY SPEC FILES"
+    puts spec.files
+    puts "=========="
+
+    Gem::Package::build spec, true
+	
+	FileUtils.mv "ripple-cli-#{@solution.options[:build_number]}.gem", "pkg/ripple-cli-#{@solution.options[:build_number]}.gem"
+	
+end
+
+
