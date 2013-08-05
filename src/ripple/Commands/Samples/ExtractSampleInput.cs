81e8fc0a0da8f717dd5a10459145a54313d6af27
jeremydmiller <jeremydmiller@yahoo.com>
2013-08-05 07:19:47 -0500

    removing the obsolete extract-samples command.  Closes GH-141

--- src/ripple/Commands/Samples/ExtractSampleInput.cs
+++
@@ -1,8 +0,0 @@
-namespace ripple.Commands.Samples
-{
-    public class ExtractSampleInput
-    {
-        public string CodeFolder { get; set; }
-        public string OutputFolder { get; set; }
-    }
-}
