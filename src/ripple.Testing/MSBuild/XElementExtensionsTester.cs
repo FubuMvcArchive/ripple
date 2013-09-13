93afa25ec7bdf87bdc423d755450ea990b0ce3ac
jmarnold <rncodepoet@gmail.com>
2013-09-13 14:55:57 -0500

    Adds the framework detection support. Closes #157 and Closes #163

---
+++ src/ripple.Testing/MSBuild/XElementExtensionsTester.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Linq;
+using FubuTestingSupport;
+using NUnit.Framework;
+using ripple.MSBuild;
+
+namespace ripple.Testing.MSBuild
+{
+    [TestFixture]
+    public class XElementExtensionsTester
+    {
+        [Test]
+        public void gets_the_value()
+        {
+            var element = new XElement("PropertyGroup");
+            element.Add(new XElement("AssemblyName", "ClassLibrary1"));
+            element.Add(new XElement("TargetFrameworkVersion", "v4.0"));
+
+            element.Get("TargetFrameworkVersion").ShouldEqual("v4.0");
+        }
+
+        [Test]
+        public void uses_the_default_value()
+        {
+            var element = new XElement("PropertyGroup");
+            element.Add(new XElement("TargetFrameworkVersion", "v4.0"));
+
+            element.Get("AssemblyName", "ClassLibrary1").ShouldEqual("ClassLibrary1");
+        }
+    }
+}
---
+++ src/ripple/MSBuild/FrameworkNameDetector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Xml.XPath;
+
+namespace ripple.MSBuild
+{
+    public class FrameworkNameDetector
+    {
+        public const string DefaultIdentifier = ".NETFramework";
+        public const string DefaultFrameworkVersion = "v4.0";
+
+        public static FrameworkName Detect(CsProjFile project)
+        {
+            var groups = project.Document.XPathSelectElements("tns:PropertyGroup", CsProjFile.Manager).ToArray();
+
+            var identifier = groups.Get("TargetFrameworkIdentifier", DefaultIdentifier);
+            var versionString = groups.Get("TargetFrameworkVersion", DefaultFrameworkVersion);
+            var profile = groups.Get("TargetFrameworkProfile");
+            var version = Version.Parse(versionString.Replace("v", "").Replace("V", ""));
+
+            return new FrameworkName(identifier, version, profile);
+        }
+    }
+}
