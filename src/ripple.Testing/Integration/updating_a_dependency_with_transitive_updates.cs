6938169f2c280783af41146586bf9943959808b8
jmarnold <rncodepoet@gmail.com>
2013-09-14 00:45:58 -0500

    Locking down #185 with tests.

--- src/ripple.Testing/Integration/updating_a_dependency_with_transitive_updates.cs
+++ src/ripple.Testing/Integration/updating_a_dependency_with_transitive_updates.cs
@@ -108,4 +108,60 @@ namespace ripple.Testing.Integration
             theSolution.FindProject("Test2").Dependencies.Has("SomethingElse").ShouldBeFalse();
         }
     }
+
+    [TestFixture]
+    public class updating_a_float_dependency_when_a_cached_version_exists
+    {
+        private SolutionGraphScenario theScenario;
+        private Solution theSolution;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+            theScenario = SolutionGraphScenario.Create(scenario =>
+            {
+                scenario.Solution("Test", test =>
+                {
+                    test.SolutionDependency("FubuTransportation", "0.9.0.1", UpdateMode.Float);
+                });
+            });
+
+            theSolution = theScenario.Find("Test");
+
+            FeedScenario.Create(scenario =>
+            {
+                scenario
+                    .For(Feed.Fubu)
+                    .Add("FubuTransportation", "0.9.0.1")
+                    .Add("FubuTransportation", "0.9.1.0");
+
+                scenario
+                    .For(theSolution.Cache.ToFeed())
+                    .Add("FubuTransportation", "0.9.0.1");
+            });
+
+            
+
+            RippleOperation
+                .With(theSolution)
+                .Execute<UpdateInput, UpdateCommand>(input =>
+                {
+                    input.NugetFlag = "FubuTransportation";
+                });
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            theScenario.Cleanup();
+            FeedRegistry.Reset();
+        }
+
+        [Test]
+        public void updates_to_the_latest()
+        {
+            theSolution.Dependencies.Find("FubuTransportation").Version.ShouldEqual("0.9.1.0");
+        }
+    }
 }
